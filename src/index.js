80591d271bf91d7cc7d7774b94186d73ebf0e920
Aniket Agrawal <aniket.mpa@gmail.com>
2023-06-11 12:43:49 +0200

    Add CSS style to show validity

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import {addValidators} from "./validators";
+import "./style.css";
 
 let emailElem = document.querySelector("input#email");
 console.log(emailElem);
 let emailValidators = {
-  "type" : "Please enter a valid email",
-  "short" : "Email addresses should be at least 5 characters long"
+  "type" : "Please enter a valid email.",
+  "short" : "Email addresses should be at least 5 characters long."
 };
+
+emailElem.setAttribute("minlength", "5");
 
 emailElem.addEventListener("input", e => {
   addValidators(e.target, emailValidators);
